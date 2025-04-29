6a618989ce9ea9b58ada21a945da8de539c0003c
Phaneendra Kumar <phaneendrakumar@Phaneendras-MacBook-Pro.local>
2025-04-29 06:58:37 +0100

    Initial commit

---
+++ calculator_server.py
@@ -0,0 +1,15 @@
+from mcp.server.fastmcp import FastMCP
+
+server = FastMCP("My calculator Server")
+
+@server.tool(name="evaluate_expression", description="Evaluates a mathematical expression and returns the result")
+def evaluate_expression(expression:str) -> float:
+    """ Evaluates a mathematical expression and returns the result."""
+    try:
+        result = eval(expression, {"__builtins__": {}}, {"sum": sum})
+        return result 
+    except Exception as e:
+        raise ValueError(f"Error evaluating expression: {e}")
+
+if __name__ == "__main__" :
+    server.run()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from mcp-starter!")
+
+
+if __name__ == "__main__":
+    main()
